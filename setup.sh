36882e666c0b710a6a7bb2cbf21f854a47b05851
cchenard <cchenard@thunderhead.com>
2018-06-13 14:56:40 -0400

    Fix crontab

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#write out current crontab
+#write out current crontabenv EDITOR=nano crontab -e
 crontab -l > mycron
 #echo new cron into cron file
-echo "* * * * * $HOME/docManager/fs.sh >> /tmp/fswatch.log 2>&1" >> mycron
+echo "* * * * * $HOME/code/docManager/fs.sh >> /tmp/cronlog.log 2>&1" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
