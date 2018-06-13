0512862116f4799a10142a801466cdad50d3655c
cchenard <cchenard@thunderhead.com>
2018-06-13 15:00:28 -0400

    Fix path

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 #write out current crontabenv EDITOR=nano crontab -e
 crontab -l > mycron
 #echo new cron into cron file
-echo "* * * * * $HOME/code/docManager/fs.sh >> /tmp/cronlog.log 2>&1" >> mycron
+echo "* * * * * $HOME/docManager/fs.sh >> /tmp/cronlog.log 2>&1" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
